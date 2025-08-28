45fdff6d09b3c44231e946c75d3235b8d620ab2b
Samriddha <samriddha97@gmail.com>
2025-08-29 01:28:31 +0530

    M Coloring Problem - wip

---
+++ DSA/recursion/_16_MColoringProblem.java
@@ -0,0 +1,28 @@
+package DSA.recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Problem Link :
+ * 1. https://takeuforward.org/data-structure/m-coloring-problem/
+ * 2. https://leetcode.com/problems/flower-planting-with-no-adjacent/description/
+ *
+ * Video(intuition) : https://www.youtube.com/watch?v=052VkKhIaQ4
+ *
+ * Topic : TRecursion, TBacktracking, TDynamicProgramming
+ * (Revisit) - Need Revision
+ *
+ *
+ *
+ */
+public class _16_MColoringProblem {
+
+    public static void main(String[] args) {
+
+        _16_MColoringProblem solution = new _16_MColoringProblem();
+
+
+    }
+
+}
