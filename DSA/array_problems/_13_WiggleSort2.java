a068b11df607b0aa22ef8a2e6dc6d3d517fc2705
Samriddha <samriddha97@gmail.com>
2025-09-02 00:33:32 +0530

    WiggleSort2 - done

--- DSA/array_problems/_13_WiggleSort2.java
+++ DSA/array_problems/_13_WiggleSort2.java
@@ -8,35 +8,71 @@ import java.util.Arrays;
  * Video :
  * Topic : TArrays
  *
+ * 324. Wiggle Sort II
+ *
+ * Given an integer array nums, reorder it such that nums[0] < nums[1] > nums[2] < nums[3]....
+ * You may assume the input array always has a valid answer.
+ *
+ * Example 1:
+ * Input: nums = [1,5,1,1,6,4]
+ * Output: [1,6,1,5,1,4]
+ * Explanation: [1,4,1,5,1,6] is also accepted.
+ *
+ * Example 2:
+ * Input: nums = [1,3,2,2,3,1]
+ * Output: [2,3,1,3,1,2]
+ *
+ * Constraints:
+ *     1 <= nums.length <= 5 * 104
+ *     0 <= nums[i] <= 5000
+ *     It is guaranteed that there will be an answer for the given input nums.
+ *
+ *
+ * Follow Up: Can you do it in O(n) time and/or in-place with O(1) extra space?
  *
 * */
 class _13_WiggleSort2 {
 
 
+    /**
+     * Time Complexity: O(n log n)
+     * Sorting: O(n log n) - dominates the complexity
+     * Array operations: O(n) - copying and reconstruction
+     *
+     * Space Complexity: O(n)
+     * Auxiliary arrays: O(n) - firstHalf and secondHalf together store all n elements
+     * Other variables: O(1)
+     *
+     * Bottom line: Sorting dominates time, auxiliary arrays dominate space.
+    * */
     public void wiggleSort(int[] nums) {
         Arrays.sort(nums);
 
         int n = nums.length;
-        int halfLength = n / 2;
+        int firstHalfLen = (n / 2)+(n%2);
+        int secondHalfLen = (n / 2);
 
         // Declare new arrays
-        int[] firstHalf = new int[halfLength];
-        int[] secondHalf = new int[halfLength];
+        int[] firstHalf = new int[firstHalfLen];
+        int[] secondHalf = new int[secondHalfLen];
 
         // Copy elements using System.arraycopy
-        System.arraycopy(nums, 0, firstHalf, 0, halfLength);
-        System.arraycopy(nums, halfLength, secondHalf, 0, halfLength);
+        System.arraycopy(nums, 0, firstHalf, 0, firstHalfLen);
+        System.arraycopy(nums, firstHalfLen, secondHalf, 0, secondHalfLen);
+
+//        System.out.println(Arrays.toString(firstHalf));
+//        System.out.println(Arrays.toString(secondHalf));
 
         int index = 0 ;
-        int i=0;
-        int j=0;
+        int i=firstHalf.length-1;
+        int j=secondHalf.length-1;
         while (index< nums.length){
             if (index%2==0){
                 nums[index] = firstHalf[i];
-                i++;
+                i--;
             }else {
                 nums[index] = secondHalf[j];
-                j++;
+                j--;
             }
             index++;
         }
@@ -47,7 +83,7 @@ class _13_WiggleSort2 {
 
         _13_WiggleSort2 solution = new _13_WiggleSort2();
 
-        int[] inputArr = new int[]{1,3,2,2,3,1};
+        int[] inputArr = new int[]{1,1,2,1,2,2,1};
         solution.wiggleSort(inputArr);
         System.out.println(Arrays.toString(inputArr));
 
