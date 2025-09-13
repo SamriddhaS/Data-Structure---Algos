8b838eb0453cd6f7bb98dc1a50ff09433b633235
Samriddha <samriddha97@gmail.com>
2025-09-14 00:42:27 +0530

    Next Greater Element II - solved

--- DSA/stack_queue/_6_NextGreaterElement2.java
+++ DSA/stack_queue/_6_NextGreaterElement2.java
@@ -5,8 +5,9 @@ import java.util.HashMap;
 import java.util.Stack;
 
 /**
- * Problem Link :
- * Video Explanation :
+ * Problem Link : https://leetcode.com/problems/next-greater-element-ii/submissions/1769743005/
+ * Video Explanation : https://www.youtube.com/watch?v=e7XQLtOQM3I
+ * (core logic is same so attaching same link of the NextGreaterElement, only the two pass thing we need to add)
  * <p>
  * 503. Next Greater Element II
  * Given a circular integer array nums (i.e., the next element of nums[nums.length - 1] is nums[0]),
@@ -64,57 +65,70 @@ public class _6_NextGreaterElement2 {
     }
 
 
-    public int[] nextGreaterElementUsingStack2(int[] nums1) {
-        int len = nums1.length;
-        Stack<Pair> stack = new Stack<>();
-        int[] answer = new int[len];
-        Arrays.fill(answer, -1);
-//        for (int i = len - 1; i >= 0; i--) {
-//            int num = nums1[i];
-//            if (stack.isEmpty()) {
-//                stack.push(new Pair(num, i));
-//            } else if (num >= stack.peek().data) {
-//                while (!stack.isEmpty() && num >= stack.peek().data) {
-//                    Pair poppedEle = stack.pop();
-//                    if (answer[poppedEle.index]==-1) answer[poppedEle.index] = num;
-//                }
-//                if(!stack.isEmpty()) {
-//                    Pair topEle = stack.peek();
-//                    answer[i] = topEle.data;
-//                }
-//                stack.push(new Pair(num, i));
-//            } else {
-//                Pair topEle = stack.peek();
-//                answer[i] = topEle.data;
-//                stack.push(new Pair(num, i));
-//            }
-//        }
-        for (int i = 0; i < len; i++) {
-            int num = nums1[i];
-            if (stack.isEmpty()) {
-                stack.push(new Pair(num, i));
-            } else if (num >= stack.peek().data) {
-                while (!stack.isEmpty() && num >= stack.peek().data) {
-                    Pair poppedEle = stack.pop();
-                    if (answer[poppedEle.index]==-1) answer[poppedEle.index] = num;
+    /**
+     * Optimal Solution :
+     * Use a decreasing monotonic stack to track elements waiting for their next greater element.
+     * Process the array twice to handle the circular nature.
+     *
+     * Algorithm Logic
+     * Two-Pass Approach:
+     *
+     * First Pass (i = 0 to n-1): Build stack, resolve elements that find their next greater element normally
+     * Second Pass (i = n to 2n-1): Use circular indexing to resolve remaining elements in stack
+     *
+     * Stack Operations:
+     * Pop: When current > stack.top(), pop smaller elements and set their answer to current
+     * Push: Only in first pass - add current element to stack for future processing
+     *
+     * Time Complexity: O(n)
+     * - Loop runs 2n times, but each element is pushed once and popped once
+     * - Total operations: n pushes + n pops = 2n = O(n)
+     *
+     * Space Complexity: O(n)
+     * - Stack can hold at most n elements (worst case: decreasing array)
+     * - Answer array: O(n)
+     * - Total: O(n)
+    * */
+    public int[] nextGreaterElementUsingStack2(int[] nums) {
+
+        Stack<Pair> decreasingMonoStack = new Stack<>();
+        int[] answers = new int[nums.length];
+        Arrays.fill(answers,-1);
+
+        for (int i=0;i<2 * nums.length;i++) {
+            int index = i % nums.length;
+            Pair current = new Pair(nums[index],index);
+            if (i<nums.length){
+                // 1st pass
+                if (decreasingMonoStack.isEmpty()) {
+                    decreasingMonoStack.push(current);
+                } else if (current.data > decreasingMonoStack.peek().data) {
+                    while (!decreasingMonoStack.isEmpty() && current.data > decreasingMonoStack.peek().data) {
+                        Pair ele = decreasingMonoStack.pop();
+                        answers[ele.index] = current.data;
+                    }
+                    decreasingMonoStack.push(current);
+                } else {
+                    decreasingMonoStack.push(current);
                 }
-                if(!stack.isEmpty()) {
-                    Pair topEle = stack.peek();
-                    answer[i] = topEle.data;
+            }else {
+                // 2st pass
+                while (!decreasingMonoStack.isEmpty() && current.data > decreasingMonoStack.peek().data) {
+                    Pair ele = decreasingMonoStack.pop();
+                    answers[ele.index] = current.data;
                 }
-                stack.push(new Pair(num, i));
-            } else {
-                Pair topEle = stack.peek();
-                answer[i] = topEle.data;
-                stack.push(new Pair(num, i));
             }
         }
-        return answer;
+
+        return answers;
     }
 
     public static void main(String[] args) {
         _6_NextGreaterElement2 obj = new _6_NextGreaterElement2();
-        int[] nums2 = {1,2,3,4,3}; // Expected - [-1,5,5,5,5] output - [-1,5,4,3,2]
+        //int[] nums2 = {1,2,3,4,3};
+        //int[] nums2 = {5,4,3,2,1};
+        int[] nums2 = {1,1,1,1,1};
+        //int[] nums2 = {1,2,3,4,5,6,5,4,5,1,2,3};
 
         System.out.println("The Answer : " + Arrays.toString(obj.nextGreaterElementUsingStack2(nums2)));
     }
